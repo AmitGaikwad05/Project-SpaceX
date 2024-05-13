51dbe799e99fbe4dfa3a7814085a8eb7fcb773c0
Amit Gaikwad <140786416+AmitGaikwad05@users.noreply.github.com>
2024-05-13 23:54:41 +0530

    Add files via upload

---
+++ js/global.js
@@ -0,0 +1,11 @@
+
+function showSideBar(){
+    const sideBar = document.getElementById("sideBar");
+    sideBar.style.display = "block";
+  }
+
+  function hideSideBar(){
+    const sideBar = document.getElementById("sideBar");
+    sideBar.style.display = "none";
+  }
+
